b3b6d1cc311b310fa6eade5737a89454ca7823b5
aakhmedkhodzhaev <teenager_08@inbox.ru>
2021-02-07 18:48:26 +0500

    Modify InfoServlet, MarketUtil

--- src/main/java/ru/job4j/carmarket/servlet/InfoServlet.java
+++ src/main/java/ru/job4j/carmarket/servlet/InfoServlet.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 
 public class InfoServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("index.do").forward(req, resp);
--- src/main/java/ru/job4j/carmarket/util/MarketUtil.java
+++ src/main/java/ru/job4j/carmarket/util/MarketUtil.java
@@ -1,4 +1,49 @@
 package ru.job4j.carmarket.util;
 
+import ru.job4j.carmarket.model.User;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class MarketUtil {
+
+    public static final String CONNECTION_NAME = "ATTRIBUTE_FOR_CONNECTION";
+
+    public static final String USER_NAME = "ATTRIBUTE_USER_NAME";
+
+    public static void saveLoginedUser(HttpSession session, User loginedUser) {
+        session.setAttribute("loginedUser", loginedUser);
+    }
+
+    public static User getLoginedUser(HttpSession session) {
+        User loginedUser = (User) session.getAttribute("loginedUser");
+        return loginedUser;
+    }
+
+    public static void saveUserCookie(HttpServletResponse resp, User user) {
+        Cookie cookieUser = new Cookie(USER_NAME, user.getEmail());
+        cookieUser.setMaxAge(24 * 60 * 60);
+        resp.addCookie(cookieUser);
+    }
+
+    public static String getUserNameInCookies(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (USER_NAME.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void deleteUserCookies(HttpServletResponse resp) {
+        Cookie cookieName = new Cookie(USER_NAME, null);
+        cookieName.setMaxAge(0);
+        resp.addCookie(cookieName);
+    }
+
 }
